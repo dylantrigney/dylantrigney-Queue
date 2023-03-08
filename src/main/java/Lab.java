b808ae67ba7f76ec74fa8d3c421a11525aad9b42
dylantrigney <dylan.rigney@revature.net>
2023-03-08 14:05:23 +0000

    queue

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -65,7 +65,7 @@ public class Lab {
      * Remove an item from a queue, and return the removed item.
      * (The method used to remove items from queues also returns the item.)
      * @param queue a Queue<String> object.
-     * @return the next String due to be removed (polled) from the Queue<String> (the oldest item in the queue.)
+     * @return the next String due be removed (polled) from the Queue<String> (the oldest item in the queue.)
      */
     public String removeFromStartOfQueue(Queue<String> queue){
         return queue.poll();
